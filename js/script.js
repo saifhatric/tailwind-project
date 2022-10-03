b36f459b5211054b8d5e9fdc9d4b98b660c22118
Saif_Hatric <saif@localhost.localdomain>
2022-10-03 14:47:01 +0100

    second commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
 const btn = document.getElementById("menu-btn");
 const nav = document.getElementById("menu");
+const boxes = document.querySelectorAll('.box')
+
+window.addEventListener("scroll", scrollBox)
 
 btn.addEventListener("click", () => {
     btn.classList.toggle("open")
     nav.classList.toggle("flex")
     nav.classList.toggle("hidden")
 })
+
+function scrollBox() {
+    const tregerBottom = (window.innerHeight / 5) * 4;
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+        if (boxTop < tregerBottom) {
+            box.classList.add("show")
+        } else {
+            box.classList.remove("show")
+        }
+    })
+}
